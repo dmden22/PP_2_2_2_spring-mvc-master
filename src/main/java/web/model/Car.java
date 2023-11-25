5315fc6edb41352f11d78d15bd1b8079cffe7686
dmden22 <maslovden22@yandex.ru>
2023-11-25 12:05:50 +0300

    Add *.iml to .gitignore, override equals() & hashcode() in model Car

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,7 @@
 package web.model;
 
+import java.util.Objects;
+
 public class Car {
 
     private String model;
@@ -44,5 +46,18 @@ public class Car {
         return  "model='" + model + '\'' +
                 ", series='" + series + '\'' +
                 ", vin='" + vin + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(model, car.model) && Objects.equals(series, car.series) && Objects.equals(vin, car.vin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, series, vin);
     }
 }
